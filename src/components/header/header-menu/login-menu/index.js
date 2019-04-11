dd7405b5e342e6e269fa3ae84f8d4d4207a070af
Diana <di.tsevan@gmail.com>
2019-04-11 15:48:25 +0300

    fixed sidebar links

--- src/components/header/header-menu/login-menu/index.js
+++ src/components/header/header-menu/login-menu/index.js
@@ -4,23 +4,10 @@ import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import { withRouter, Link } from 'react-router-dom';
 import { FaUserCircle } from 'react-icons/fa';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
-import { logInUser, resetUserInfo } from '../../../../redux/user/actions';
-import { resetTicketsInfo } from '../../../../redux/search/tickets/actions';
-import { resetSelectedFlightInfo } from '../../../../redux/user/selectedFlight/actions';
-import { resetReturnSelectedFlightInfo } from '../../../../redux/user/returnSelectedFlight/actions';
-import { resetOrdersInfo } from '../../../../redux/user/orders/actions';
 
 class LoginMenu extends React.Component {
   static propTypes = {
-    logInUser: PropTypes.func.isRequired,
-    history: PropTypes.object.isRequired,
-    resetTicketsInfo: PropTypes.func.isRequired,
-    resetSelectedFlightInfo: PropTypes.func.isRequired,
-    resetReturnSelectedFlightInfo: PropTypes.func.isRequired,
-    resetOrdersInfo: PropTypes.func.isRequired,
-    resetUserInfo: PropTypes.func.isRequired,
+    logOut: PropTypes.func.isRequired,
   };
 
   state = {
@@ -32,16 +19,6 @@ class LoginMenu extends React.Component {
   handleClose = () => {
     this.setState({ anchorEl: null });
   };
-
-  logOut = () => {
-    localStorage.clear();
-    this.props.resetTicketsInfo();
-    this.props.resetSelectedFlightInfo();
-    this.props.resetReturnSelectedFlightInfo();
-    this.props.resetOrdersInfo();
-    this.props.resetUserInfo();
-    this.props.logInUser(false);
-  }
 
   render() {
     const { anchorEl } = this.state;
@@ -65,7 +42,7 @@ class LoginMenu extends React.Component {
               onClose={this.handleClose}
             >
               <MenuItem onClick={this.handleClose}>
-                <Link to="/search" className="login-menu__link-to" onClick={this.logOut}>
+                <Link to="/search" className="login-menu__link-to" onClick={this.props.logOut}>
                   Logout
                 </Link>
               </MenuItem>
@@ -90,22 +67,10 @@ class LoginMenu extends React.Component {
               </MenuItem>
             </Menu>
           )
-          }
+        }
       </div>
     );
   }
 }
 
-const mapDispatchToProps = dispatch => ({
-  logInUser: bool => dispatch(logInUser(bool)),
-  resetTicketsInfo: () => dispatch(resetTicketsInfo()),
-  resetSelectedFlightInfo: () => dispatch(resetSelectedFlightInfo()),
-  resetReturnSelectedFlightInfo: () => dispatch(resetReturnSelectedFlightInfo()),
-  resetOrdersInfo: () => dispatch(resetOrdersInfo()),
-  resetUserInfo: () => dispatch(resetUserInfo()),
-});
-
-export default compose(
-  connect(null, mapDispatchToProps),
-  withRouter,
-)(LoginMenu);
+export default withRouter(LoginMenu);
--- src/components/header/header-menu/menu/index.js
+++ src/components/header/header-menu/menu/index.js
@@ -1,40 +1,105 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { logInUser, resetUserInfo } from '../../../../redux/user/actions';
+import { resetTicketsInfo } from '../../../../redux/search/tickets/actions';
+import { resetSelectedFlightInfo } from '../../../../redux/user/selectedFlight/actions';
+import { resetReturnSelectedFlightInfo } from '../../../../redux/user/returnSelectedFlight/actions';
+import { resetOrdersInfo } from '../../../../redux/user/orders/actions';
 import LoginMenu from '../login-menu';
 import './index.scss';
 
-const Menu = props => (
-  <div className={props.className}>
-    <Link
-      to="/search"
-      className="link-to"
-      onClick={props.onClick}
-    >
-      Search
-    </Link>
+function Menu(props) {
+  Menu.propTypes = {
+    className: PropTypes.any.isRequired,
+    loggedInUser: PropTypes.bool.isRequired,
+    onClick: PropTypes.func.isRequired,
+    logInUser: PropTypes.func.isRequired,
+    resetTicketsInfo: PropTypes.func.isRequired,
+    resetSelectedFlightInfo: PropTypes.func.isRequired,
+    resetReturnSelectedFlightInfo: PropTypes.func.isRequired,
+    resetOrdersInfo: PropTypes.func.isRequired,
+    resetUserInfo: PropTypes.func.isRequired,
+  };
 
-    {props.loggedInUser && (
+  const logOut = () => {
+    localStorage.clear();
+    props.resetTicketsInfo();
+    props.resetSelectedFlightInfo();
+    props.resetReturnSelectedFlightInfo();
+    props.resetOrdersInfo();
+    props.resetUserInfo();
+    props.logInUser(false);
+  };
+
+  const showLinks = () => {
+    if (props.onClick) {
+      if (props.loggedInUser) {
+        return (
+          <>
+            <Link
+              to="/orders-history"
+              className="link-to"
+              onClick={props.onClick}
+            >
+              My orders
+            </Link>
+            <Link to="/search" className="link-to" onClick={logOut}>
+              Logout
+            </Link>
+          </>
+        );
+      }
+
+      return (
+        <>
+          <Link
+            to="/login"
+            className="link-to"
+            onClick={props.onClick}
+          >
+            Log In
+          </Link>
+          <Link
+            to="/register"
+            className="link-to"
+            onClick={props.onClick}
+          >
+            Register
+          </Link>
+        </>
+      );
+    }
+
+    return <LoginMenu logOut={logOut} />;
+  };
+
+  return (
+    <div className={props.className}>
       <Link
-        to="/orders-history"
+        to="/search"
         className="link-to"
         onClick={props.onClick}
       >
-        My orders
+        Search
       </Link>
-    )}
-    <LoginMenu />
-  </div>
-);
-
-Menu.propTypes = {
-  className: PropTypes.any.isRequired,
-  loggedInUser: PropTypes.bool.isRequired,
-};
+      {showLinks()}
+    </div>
+  );
+}
 
 const mapStateToProps = state => ({
   loggedInUser: state.user.requestInfo.loggedInUser,
 });
 
-export default connect(mapStateToProps)(Menu);
+const mapDispatchToProps = dispatch => ({
+  logInUser: bool => dispatch(logInUser(bool)),
+  resetTicketsInfo: () => dispatch(resetTicketsInfo()),
+  resetSelectedFlightInfo: () => dispatch(resetSelectedFlightInfo()),
+  resetReturnSelectedFlightInfo: () => dispatch(resetReturnSelectedFlightInfo()),
+  resetOrdersInfo: () => dispatch(resetOrdersInfo()),
+  resetUserInfo: () => dispatch(resetUserInfo()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Menu);
